9f3152cfd12174c8ede60fe52e852fd2eb92baf5
tsl-ramkrishna <ram_krishna.shrestha@tsl.ac.uk>
2019-10-17 14:04:40 +0000

    bash script to run fastqc and kallisto

---
+++ analysis.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# run fastqc and kallisto on all fastq files
+# requirements:
+# fastqc-0.11.5
+# kallisto-0.43.1
+
+function fastqc(){
+	sbatch --mem 6G -o /dev/null --wrap "source fastqc-0.11.5; fastqc -o results/$1 --extract -f fastq $2"
+}
+
+function kallisto(){
+	sbatch --mem 10G -o results/$1/${3}_kallisto.log  -J $3 -n 2 --wrap "source kallisto-0.43.1; kallisto quant -i /hpc-home/dingp/kallisto_Ram/AtRTDv2_QUASI_kallisto_index_v2  -o results/$1/${3}_kallisto -b 100 --single -l 170 -s 10 $2"
+}
+
+for filename in /tsl/data/reads/jjones/dingp_rnaseq_bgiseq500_seti_eti_2018/*/*/raw/*.fq.gz; do
+	sample=$(dirname $filename | sed 's%/tsl/data/reads/jjones/dingp_rnaseq_bgiseq500_seti_eti_2018%%' | sed 's%/% %g' | awk '{print $1}')
+	identifier=$(basename $filename | sed 's/.fq.gz//')
+	if [[ "$@" =~ "fastqc" ]]; then
+		fastqc $sample $filename
+	fi
+
+	if [[ "$@" =~ "kallisto" ]]; then
+		kallisto $sample $filename $identifier
+	fi
+done
+
+
+		
